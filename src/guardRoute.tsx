5686ce1dfdeccd9b190fa14c4fdb117e82910695
Thorlani <tolanipopoola07@gmail.com>
2023-01-20 23:05:02 +0100

    wip

--- src/guardRoute.tsx
+++ src/guardRoute.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import cookies from "js-cookie";
 import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 
-const useAuth = () => {
-  const user = cookies.get("user");
-  return user;
-};
 
 const GuardRoute = () => {
-  const parameter = useSelector((state: any) => state.auth.parameter);
-  const isAuthenticated = useAuth();
+  const user = cookies.get("user");
 
-  return isAuthenticated || parameter ? <Outlet /> : <Navigate to="/" />;
+  console.log(user);
+  
+
+  return user ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default GuardRoute;
