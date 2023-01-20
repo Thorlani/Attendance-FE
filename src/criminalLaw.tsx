fc87db3e695362535d1c09dca54fac97d566b510
Thorlani <tolanipopoola07@gmail.com>
2023-01-20 11:20:40 +0100

    wip

--- src/criminalLaw.tsx
+++ src/criminalLaw.tsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import { DATA } from "./data";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useQuery } from "react-query";
 
 const CriminalLaw = () => {
   const navigate = useNavigate();
-  const data = DATA;
+  // const data = DATA;
 
   type input = {
     appearance: number;
@@ -73,6 +74,24 @@ const CriminalLaw = () => {
       });
   };
 
+  const {
+    data: ctAttendance,
+    isLoading,
+    isError,
+  } = useQuery(
+    "getting-ct-attendance",
+    () => axios.get("https://attendance-be.vercel.app/api/getCl"),
+    {
+      cacheTime: 600_000,
+      staleTime: 500_000,
+    }
+  );
+
+  const data = ctAttendance?.data;
+
+  type setDisplayStage = number;
+  const [isDisplayStage, setIsDisplayStage] = useState<setDisplayStage>(0);
+
   return (
     <div className="container" style={{ width: "100%" }}>
       <nav>
@@ -90,119 +109,145 @@ const CriminalLaw = () => {
         </ul>
       </nav>
       <h1>Criminal law</h1>
-      <p>Attendance</p>
-      <div>
-        <input
-          type="number"
-          name="appearance"
-          value={totalAppearance.appearance}
-          onChange={handleChange}
-          style={{ width: "180px" }}
-        />
-        <a
-          href="#"
-          role="button"
-          style={{ marginLeft: "10px" }}
-          onClick={handleClick}
-        >
-          Enter
-        </a>
-      </div>
-      <div style={{ width: "100%", height: "fit", marginTop: "20px" }}>
-        <figure>
-          <table style={{ width: "100%" }}>
-            <thead>
-              <tr style={{ width: "100%", textAlign: "center" }}>
-                <th>Matric No</th>
-                <th>Name</th>
-                <th>Total App.</th>
-                <th>Cutoff Mark(66%)</th>
-              </tr>
-            </thead>
-            <tbody style={{ textAlign: "center" }}>
-              {data.map(
-                (item: {
-                  matric: string;
-                  name: string;
-                  date: string;
-                  signature: string;
-                }) => {
-                  return (
-                    <tr>
-                      <td>{item.matric}</td>
-                      <td>{item.signature}</td>
-                      <td>
-                        {
-                          data.filter(
-                            (obj: {
-                              matric: string;
-                              name: string;
-                              date: string;
-                              signature: string;
-                            }) => {
-                              return obj.matric === item.matric;
-                            }
-                          ).length
-                        }
-                      </td>
-                      {totalAppearance.isDisplay && (
-                        <td>
-                          {(data.filter(
-                            (obj: {
-                              matric: string;
-                              name: string;
-                              date: string;
-                              signature: string;
-                            }) => {
-                              return obj.matric === item.matric;
-                            }
-                          ).length /
-                            totalAppearance.appearance) *
-                            100}
-                        </td>
-                      )}
-                    </tr>
-                  );
-                }
-              )}
-            </tbody>
-          </table>
-        </figure>
-      </div>
-      <div>
-        <form>
+      {isDisplayStage === 0 ? (
+        <>
+          <p>What would you live to do?</p>
           <div>
-            <label htmlFor="matric">Matric Number</label>
+            <button type="button" onClick={() => setIsDisplayStage(2)}>
+              Sign Attendance
+            </button>
+            <button type="button" onClick={() => setIsDisplayStage(1)}>
+              Check Attendance
+            </button>
+          </div>
+        </>
+      ) : isDisplayStage === 1 ? (
+        <div style={{display: "flex"}}>
+          <p>List of Attendance</p>
+          <a href="#" role={"banner"} style={{marginLeft: "15px"}} onClick={() => setIsDisplayStage(0)}>go back</a>
+        </div>
+      ) : (
+        <div style={{display: "flex"}}>
+          <p>Sign your Attendance</p>
+          <a href="#" role={"banner"} style={{marginLeft: "15px"}} onClick={() => setIsDisplayStage(0)}>go back</a>
+        </div>
+      )}
+      {isDisplayStage === 1 && (
+        <>
+          <div>
+            <label htmlFor="setTotalAppearance">Set Total Appearance</label>
             <input
               type="number"
-              name="matric"
-              value={formData.matric}
-              onChange={handleChangeTwo}
+              name="appearance"
+              value={totalAppearance.appearance}
+              onChange={handleChange}
+              style={{ width: "180px" }}
             />
+            <a
+              href="#"
+              role="button"
+              style={{ marginLeft: "10px" }}
+              onClick={handleClick}
+            >
+              Enter
+            </a>
           </div>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChangeTwo}
-            />
-          </div>
-          <div>
-            <label htmlFor="imagePath">Photo</label>
-            <input
-              type="file"
-              name="imagePath"
-              value={formData.imagePath}
-              onChange={handleChangeTwo}
-            />
-          </div>
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </form>
-      </div>
+          {isError === true ? (
+            <p>Table content cannot be fetched due to network error</p>
+          ) : isLoading === true ? (
+            <p>Table content is loading currently</p>
+          ) : (
+            <div style={{ width: "100%", height: "fit", marginTop: "20px" }}>
+              <figure>
+                <table style={{ width: "100%" }}>
+                  <thead>
+                    <tr style={{ width: "100%", textAlign: "center" }}>
+                      <th>Matric No</th>
+                      <th>Name</th>
+                      <th>Total App.</th>
+                      <th>Cutoff Mark(66%)</th>
+                    </tr>
+                  </thead>
+                  <tbody style={{ textAlign: "center" }}>
+                    {data.map((item: { matric: string; name: string }) => {
+                      return (
+                        <tr>
+                          <td>{item?.matric}</td>
+                          <td>{item?.name}</td>
+                          <td>
+                            {
+                              data.filter(
+                                (obj: {
+                                  matric: string;
+                                  name: string;
+                                  date: string;
+                                }) => {
+                                  return obj.matric === item.matric;
+                                }
+                              ).length
+                            }
+                          </td>
+                          {totalAppearance.isDisplay && (
+                            <td>
+                              {(data.filter(
+                                (obj: {
+                                  matric: string;
+                                  name: string;
+                                  date: string;
+                                }) => {
+                                  return obj.matric === item.matric;
+                                }
+                              ).length /
+                                totalAppearance.appearance) *
+                                100}
+                            </td>
+                          )}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </figure>
+            </div>
+          )}
+        </>
+      )}
+      {isDisplayStage === 2 && (
+        <div>
+          <form>
+            <div>
+              <label htmlFor="matric">Matric Number</label>
+              <input
+                type="number"
+                name="matric"
+                value={formData.matric}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <div>
+              <label htmlFor="imagePath">Photo</label>
+              <input
+                type="file"
+                name="imagePath"
+                value={formData.imagePath}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
--- src/ct.tsx
+++ src/ct.tsx
@@ -7,7 +7,7 @@ import "@picocss/pico";
 
 const CommercialTransaction = () => {
   const navigate = useNavigate();
-  const data = DATA;
+  // const data = DATA;
 
   type input = {
     appearance: number;
@@ -61,7 +61,7 @@ const CommercialTransaction = () => {
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
     axios
-      .post("http://localhost:3005/api/postCt", {
+      .post("https://attendance-be.vercel.app/api/postCt", {
         matric: formData.matric,
         name: formData.name,
         imagePath: formData.imagePath,
@@ -74,17 +74,23 @@ const CommercialTransaction = () => {
       });
   };
 
-  const { data: ctAttendance } = useQuery("getting-ct-attendance", () =>
-    axios.get("http://localhost:3005/api/getCt"),
+  const {
+    data: ctAttendance,
+    isLoading,
+    isError,
+  } = useQuery(
+    "getting-ct-attendance",
+    () => axios.get("https://attendance-be.vercel.app/api/getCt"),
     {
-      staleTime: 500_000
+      cacheTime: 600_000,
+      staleTime: 500_000,
     }
   );
 
-  const dat = ctAttendance?.data;
+  const data = ctAttendance?.data;
 
-  console.log(dat);
-  
+  type setDisplayStage = number;
+  const [isDisplayStage, setIsDisplayStage] = useState<setDisplayStage>(0);
 
   return (
     <div className="container" style={{ width: "100%" }}>
@@ -103,114 +109,159 @@ const CommercialTransaction = () => {
         </ul>
       </nav>
       <h1>Commercial Transaction</h1>
-      <p>Attendance</p>
-      <div>
-        <input
-          type="number"
-          name="appearance"
-          value={totalAppearance.appearance}
-          onChange={handleChange}
-          style={{ width: "180px" }}
-        />
-        <a
-          href="#"
-          role="button"
-          style={{ marginLeft: "10px" }}
-          onClick={handleClick}
-        >
-          Enter
-        </a>
-      </div>
-      <div style={{ width: "100%", height: "fit", marginTop: "20px" }}>
-        <figure>
-          <table style={{ width: "100%" }}>
-            <thead>
-              <tr style={{ width: "100%", textAlign: "center" }}>
-                <th>Matric No</th>
-                <th>Name</th>
-                <th>Total App.</th>
-                <th>Cutoff Mark(66%)</th>
-              </tr>
-            </thead>
-            <tbody style={{ textAlign: "center" }}>
-              {data.map(
-                (item: { matric: string; name: string; date: string }) => {
-                  return (
-                    <tr>
-                      <td>{item?.matric}</td>
-                      <td>{item?.name}</td>
-                      <td>
-                        {
-                          data.filter(
-                            (obj: {
-                              matric: string;
-                              name: string;
-                              date: string;
-                              signature: string;
-                            }) => {
-                              return obj.matric === item.matric;
-                            }
-                          ).length
-                        }
-                      </td>
-                      {totalAppearance.isDisplay && (
-                        <td>
-                          {(data.filter(
-                            (obj: {
-                              matric: string;
-                              name: string;
-                              date: string;
-                              signature: string;
-                            }) => {
-                              return obj.matric === item.matric;
-                            }
-                          ).length /
-                            totalAppearance.appearance) *
-                            100}
-                        </td>
-                      )}
-                    </tr>
-                  );
-                }
-              )}
-            </tbody>
-          </table>
-        </figure>
-      </div>
-      <div>
-        <form>
+      {isDisplayStage === 0 ? (
+        <>
+          <p>What would you live to do?</p>
           <div>
-            <label htmlFor="matric">Matric Number</label>
+            <button type="button" onClick={() => setIsDisplayStage(2)}>
+              Sign Attendance
+            </button>
+            <button type="button" onClick={() => setIsDisplayStage(1)}>
+              Check Attendance
+            </button>
+          </div>
+        </>
+      ) : isDisplayStage === 1 ? (
+        <div style={{ display: "flex" }}>
+          <p>List of Attendance</p>
+          <a
+            href="#"
+            role={"banner"}
+            style={{ marginLeft: "15px" }}
+            onClick={() => setIsDisplayStage(0)}
+          >
+            go back
+          </a>
+        </div>
+      ) : (
+        <div style={{ display: "flex" }}>
+          <p>Sign your Attendance</p>
+          <a
+            href="#"
+            role={"banner"}
+            style={{ marginLeft: "15px" }}
+            onClick={() => setIsDisplayStage(0)}
+          >
+            go back
+          </a>
+        </div>
+      )}
+      {isDisplayStage === 1 && (
+        <>
+          <div>
+            <label htmlFor="setTotalAppearance">Set Total Appearance</label>
             <input
               type="number"
-              name="matric"
-              value={formData.matric}
-              onChange={handleChangeTwo}
+              name="appearance"
+              value={totalAppearance.appearance}
+              onChange={handleChange}
+              style={{ width: "180px" }}
             />
+            <a
+              href="#"
+              role="button"
+              style={{ marginLeft: "10px" }}
+              onClick={handleClick}
+            >
+              Enter
+            </a>
           </div>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChangeTwo}
-            />
-          </div>
-          <div>
-            <label htmlFor="imagePath">Photo</label>
-            <input
-              type="file"
-              name="imagePath"
-              value={formData.imagePath}
-              onChange={handleChangeTwo}
-            />
-          </div>
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </form>
-      </div>
+          {isError === true ? (
+            <p>Table content cannot be fetched due to network error</p>
+          ) : isLoading === true ? (
+            <p>Table content is loading currently</p>
+          ) : (
+            <div style={{ width: "100%", height: "fit", marginTop: "20px" }}>
+              <figure>
+                <table style={{ width: "100%" }}>
+                  <thead>
+                    <tr style={{ width: "100%", textAlign: "center" }}>
+                      <th>Matric No</th>
+                      <th>Name</th>
+                      <th>Total App.</th>
+                      <th>Cutoff Mark(66%)</th>
+                    </tr>
+                  </thead>
+                  <tbody style={{ textAlign: "center" }}>
+                    {data.map((item: { matric: string; name: string }) => {
+                      return (
+                        <tr>
+                          <td>{item?.matric}</td>
+                          <td>{item?.name}</td>
+                          <td>
+                            {
+                              data.filter(
+                                (obj: {
+                                  matric: string;
+                                  name: string;
+                                  date: string;
+                                }) => {
+                                  return obj.matric === item.matric;
+                                }
+                              ).length
+                            }
+                          </td>
+                          {totalAppearance.isDisplay && (
+                            <td>
+                              {(data.filter(
+                                (obj: {
+                                  matric: string;
+                                  name: string;
+                                  date: string;
+                                }) => {
+                                  return obj.matric === item.matric;
+                                }
+                              ).length /
+                                totalAppearance.appearance) *
+                                100}
+                            </td>
+                          )}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </figure>
+            </div>
+          )}
+        </>
+      )}
+      {isDisplayStage === 2 && (
+        <div>
+          <form>
+            <div>
+              <label htmlFor="matric">Matric Number</label>
+              <input
+                type="number"
+                name="matric"
+                value={formData.matric}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <div>
+              <label htmlFor="imagePath">Photo</label>
+              <input
+                type="file"
+                name="imagePath"
+                value={formData.imagePath}
+                onChange={handleChangeTwo}
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
